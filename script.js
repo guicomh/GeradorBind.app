34cd319d72c64816da940814f0a601079f512589
Guilherme <eliseuvasconcellos@gmail.com>
2024-01-14 17:49:17 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+function gerarBind() {
+    // Obter valores selecionados
+    var tecla = document.getElementById("opcao").value;
+    var animacao = document.getElementById("anime").value;
+
+    // Construir o comando Bind
+    var comandoBind = 'bind keyboard "' + tecla + '" "' + animacao + '"'
+
+    // Exibir o resultado na textarea
+    document.getElementById("resultado").value = comandoBind;
+}
