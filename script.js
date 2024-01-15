5a8be76d1e45e053682aaad04eda0338dd86d5f9
Guilherme <eliseuvasconcellos@gmail.com>
2024-01-15 19:00:04 -0300

    update

--- script.js
+++ script.js
@@ -10,4 +10,32 @@ function gerarBind() {
 
     // Exibir o resultado na textarea
     document.getElementById("resultado").value = comandoBind;
+
+}
+
+function copiarResultado() {
+    // Selecionar o conteúdo da textarea
+    var resultadoTextarea = document.getElementById("resultado");
+    resultadoTextarea.select();
+
+    // Executar o comando de cópia
+    document.execCommand("copy");
+
+    // Exibir um alerta indicando que o resultado foi copiado
+    alert("Copiado com sucesso! Agora aperte F8 no FiveM e cole.");
+}
+
+
+function ApagarBind() {
+    // Obter valores selecionados
+    var tecla = document.getElementById("opcao").value;
+    var animacao = document.getElementById("anime").value;
+
+    // Construir o comando Bind
+    var comandoBind = 'unbind keyboard "' + tecla + '"'
+    
+
+    // Exibir o resultado na textarea
+    document.getElementById("resultado").value = comandoBind;
+
 }
